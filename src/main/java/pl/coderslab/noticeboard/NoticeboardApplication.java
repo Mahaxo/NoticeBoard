667cb78ffae52e6a9b1777e2fce850f34fecaa8a
Maximilian Sydry <maximilian.sydry@gmail.com>
2019-01-05 11:24:30 +0100

    First commit

---
+++ src/main/java/pl/coderslab/noticeboard/NoticeboardApplication.java
@@ -0,0 +1,14 @@
+package pl.coderslab.noticeboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoticeboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NoticeboardApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/pl/coderslab/noticeboard/NoticeboardApplicationTests.java
@@ -0,0 +1,17 @@
+package pl.coderslab.noticeboard;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NoticeboardApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
